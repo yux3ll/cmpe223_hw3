12a4078246f86798c965274d08ed0e7a0d2a5720
yux3ll <yuksel.baypinar@tedu.edu.tr>
2022-12-16 13:03:39 +0300

    q2 vpl issues

--- src/part2/BinarySearchTree.java
+++ src/part2/BinarySearchTree.java
@@ -1,111 +1,177 @@
 package part2;
-
 public class BinarySearchTree {
-     private Node root;
+    private Node root;
 
-     private class Node{
+    private class Node{
 
-         Node left;
-         Node right;
+        Node left;
+        Node right;
 
-         int key;
-         int ratingStar;
-         String name;
-         boolean isAvailable;
+        int key;
+        int ratingStar;
+        String name;
+        boolean isAvailable;
 
-         public Node(int key, String name, int ratingStar, boolean isAvailable) {
-             this.key = key;
-             this.name = name;
-             this.ratingStar = ratingStar;
-             this.isAvailable = isAvailable;
-         }
-
-     }
-
-        public BinarySearchTree() {
-            root = null;
+        public Node(int key, String name, int ratingStar, boolean isAvailable) {
+            this.key = key;
+            this.name = name;
+            this.ratingStar = ratingStar;
+            this.isAvailable = isAvailable;
         }
 
-        public void addCaptain(int key, String name) {
-            System.out.println("Add Captain: Add a new captain record in the System \n");
-            root = addCaptain(root, key, name);
-            System.out.println("----------------------------------------------------------------");
-        }
-        private Node addCaptain(Node node, int key, String name) {
-            if (node == null) {
-                node = new Node(key, name, 0,true);
-                System.out.println("\t\t\tID: " + key + "\n\t\t\tName: " + name + "\n\t\t\tAvailable: True\n\t\t\tRating star: 0");
+    }
+
+    public BinarySearchTree() {
+        root = null;
+    }
+
+    public void addCaptain(int key, String name) {
+        System.out.println("Add Captain: Add a new captain record in the System\n");
+        root = addCaptain(root, key, name);
+        System.out.println("----------------------------------------------------------------");
+    }
+    private Node addCaptain(Node node, int key, String name) {
+        if (node == null) {
+            node = new Node(key, name, 0,true);
+            System.out.println("\t\t\tID: " + key + "\n\t\t\tName: " + name + "\n\t\t\tAvailable: True\n\t\t\tRating star: 0");
+        } else {
+            if (key <= node.key) {
+                node.left = addCaptain(node.left, key, name);
             } else {
-                if (key <= node.key) {
-                    node.left = addCaptain(node.left, key, name);
-                } else {
-                    node.right = addCaptain(node.right, key, name);
-                }
+                node.right = addCaptain(node.right, key, name);
             }
+        }
+        return node;
+    }
+
+
+
+    public void printCaptain(int key) {
+        System.out.print("Display Captain:");
+        printCaptain(root, key);
+        System.out.println("----------------------------------------------------------------");
+
+    }
+    private Node printCaptain(Node node, int key) {
+        if (node == null) {
+            System.out.println(" Couldn't find any captain with ID number " + key);
+            return null;
+        } else {
+            if (key < node.key) {
+                node.left = printCaptain(node.left, key);
+            } else if (key > node.key) {
+                node.right = printCaptain(node.right, key);
+            } else {
+                System.out.println("\n\n\t\t\tID: " + node.key + "\n\t\t\tName: " + node.name + "\n\t\t\tAvailable: " + node.isAvailable + "\n\t\t\tRating star:" + node.ratingStar);
+
+            }
+        }
+        return node;
+    }
+
+    public void printAllCaptain() {
+        System.out.println("----------ALL CAPTAINS----------");
+        printAllCaptain(root);
+    }
+    private void printAllCaptain(Node node) {
+        if (node != null) {
+            printAllCaptain(node.left);
+            System.out.println("--CAPTAIN:\n\n\t\t\tID: " + node.key + "\n\t\t\tName: " + node.name + "\n\t\t\tAvailable: " + node.isAvailable + "\n\t\t\tRating star: " + node.ratingStar + "\n");
+            printAllCaptain(node.right);
+        }
+    }
+    private Node findMin(Node node) {
+        if (node == null) {
+            return null;
+        } else if (node.left == null) {
             return node;
+        } else {
+            return findMin(node.left);
         }
+    }
 
-        public void deleteCaptain(int key) {
-            System.out.print("Delete Captain:");
-            root = deleteCaptain(root, key);
-        }
-        private Node deleteCaptain(Node node, int key) {
-            if (node == null) {
-                System.out.println(" Couldn't find any captain with ID number");
-                return null;
+    public void startRide(int key) {
+        System.out.print("IsAvailable: ");
+        root = startRide(root, key);
+        System.out.println("\n----------------------------------------------------------------");
+    }
+
+    private Node startRide(Node node, int key) {
+        if (node == null) {
+            System.out.println("Couldn't find any captain with ID number " + key);
+            return null;
+        } else {
+            if (key < node.key) {
+                node.left = startRide(node.left, key);
+            } else if (key > node.key) {
+                node.right = startRide(node.right, key);
             } else {
-                if (key < node.key) {
-                    node.left = deleteCaptain(node.left, key);
-                } else if (key > node.key) {
-                    node.right = deleteCaptain(node.right, key);
-                } else {
-                    if (node.left == null && node.right == null) {
-                        node = null;
-                    } else if (node.left == null) {
-                        node = node.right;
-                    } else if (node.right == null) {
-                        node = node.left;
-                    } else {
-                        Node temp = findMin(node.right);
-                        node.key = temp.key;
-                        node.right = deleteCaptain(node.right, temp.key);
-                    }
-                    System.out.println("The captain left CDRC");
+                if(node.isAvailable) {
+                    node.isAvailable = false;
+                    System.out.println("Reserve a new Ride with captain " + key);
+                }else System.out.println("The captain " + node.name + " is not available. He is on another ride!");
+            }
+        }
+        return node;
+    }
+
+    public void endRide(int key, int ratingStar) {
+        System.out.print("Finish: ");
+        root = endRide(root, key, ratingStar);
+        System.out.println("----------------------------------------------------------------");
+    }
+
+    private Node endRide(Node node, int key, int ratingStar) {
+        if (node == null) {
+            System.out.println("Couldn't find any captain with ID number " + key);
+            return null;
+        } else {
+            if (key < node.key) {
+                node.left = endRide(node.left, key, ratingStar);
+            } else if (key > node.key) {
+                node.right = endRide(node.right, key, ratingStar);
+            } else {
+                if(!node.isAvailable) {
+                    node.isAvailable = true;
+                    node.ratingStar = ratingStar;
+                    System.out.println("Finish ride with captain " + key);
+                    System.out.println("\n\t\t\tID: " + node.key + "\n\t\t\tName: " + node.name + "\n\t\t\tAvailable: " + node.isAvailable + "\n\t\t\tRating star: " + node.ratingStar);
+                }else System.out.println("The captain " + node.name + " is not in a ride");
+            }
+        }
+        return node;
+    }
+
+
+    public void deleteCaptain(int key){
+        System.out.print("Delete Captain:");
+        root = deleteCaptain(root, key);
+        System.out.println("----------------------------------------------------------------");
+    }
+    private Node deleteCaptain(Node node, int key){
+        if(node == null){
+            System.out.println(" Couldn't find any captain with ID number " + key);
+            return null;
+        }else{
+            if(key < node.key){
+                node.left = deleteCaptain(node.left, key);
+            }else if(key > node.key){
+                node.right = deleteCaptain(node.right, key);
+            }else{
+                if(node.left == null && node.right == null){
+                    node = null;
+                }else if(node.left == null){
+                    node = node.right;
+                }else if(node.right == null){
+                    node = node.left;
+                }else{
+                    Node temp = findMin(node.right);
+                    node.key = temp.key;
+                    node.right = deleteCaptain(node.right, temp.key);
                 }
-            }
-            return node;
-        }
-
-        public void printCaptain(int key) {
-            printCaptain(root, key);
-        }
-        private Node printCaptain(Node node, int key) {
-            if (node == null) {
-                System.out.println("Couldn't find any captain with ID number");
-                return null;
-            } else {
-                if (key < node.key) {
-                    node.left = printCaptain(node.left, key);
-                } else if (key > node.key) {
-                    node.right = printCaptain(node.right, key);
-                } else {
-                    System.out.println("Captain's name: " + node.name);
-                    System.out.println("Captain's ID number: " + node.key);
-                    System.out.println("Captain's rating: " + node.ratingStar);
-                    System.out.println("Captain's availability: " + node.isAvailable);
-                }
-            }
-            return node;
-        }
-
-        private Node findMin(Node node) {
-            if (node == null) {
-                return null;
-            } else if (node.left == null) {
-                return node;
-            } else {
-                return findMin(node.left);
+                System.out.println("The captain " + node.name + " left CCR");
             }
         }
-
+        return node;
+    }
 }
--- src/part2/Driver.java
+++ src/part2/Driver.java
@@ -1,22 +1,20 @@
 package part2;
 
-import part1.BinarySearchTree;
-
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Scanner;
 
 public class Driver {
     public static void main(String[] args) throws FileNotFoundException {
-        int key, quantity;
+        int key, ratingStar;
         String name;
-        part1.BinarySearchTree tree = new BinarySearchTree();
+        BinarySearchTree tree = new BinarySearchTree();
 
         Scanner sc = new Scanner(System.in);
         String filePath = sc.nextLine();
         Scanner input = new Scanner(new File(filePath));
 
-        System.out.println("--------------- WELCOME TO ITS SYSTEM ---------------");
+        System.out.println("--------------- WELCOME TO CDRC SYSTEM ---------------");
 
 
 
@@ -24,24 +22,41 @@ public class Driver {
         while(true) {
             String temp = input.next();
             switch (temp) {
-                case "Add_product":
-                    System.out.println("Create Product: \n");
+                case "Add_Captain":
                     key = input.nextInt();
                     name = input.next();
-                    quantity = input.nextInt();
-                    tree.addProduct(key, name, quantity);
+                    tree.addCaptain(key, name);
                     input.nextLine();
                     break;
                 case "Is_Available":
                     key = input.nextInt();
-                    tree.searchProduct(key);
+                    tree.startRide(key);
+                    input.nextLine();
+                    break;
+                case "Finish":
+                    key = input.nextInt();
+                    ratingStar = input.nextInt();
+                    tree.endRide(key, ratingStar);
+                    input.nextLine();
+                    break;
+                case "Display_captain":
+                    key = input.nextInt();
+                    tree.printCaptain(key);
+                    input.nextLine();
+                    break;
+                case "Display_all_captains":
+                    tree.printAllCaptain();
+                    input.nextLine();
+                    break;
+                    case "Delete_captain":
+                    key = input.nextInt();
+                    tree.deleteCaptain(key);
                     input.nextLine();
                     break;
                 case "Quit":
-                    System.out.println("Thank you for using ITS, Good Bye!");
+                    System.out.println("Thank you for using CDRC, Good Bye!");
                     System.exit(0);
                     break;
-
                 default:
                     System.out.println("Invalid operation!");
                     break;
